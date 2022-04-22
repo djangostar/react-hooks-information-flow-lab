52e3760da14ea6fd5ee999429c19ce77dee67482
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-04-21 21:41:15 -0700

    commit tree 6b495a088abadf83d1e1f69efcf61af07e594915

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,13 @@
 import React from "react"
 
-/* Deliverables
-    refractor the <header> element out of App.js
-    
-    clicking the <button> inside the Header component should toggle
-    dark mode on or off
-
-    pass callback function as prop called onDarkModeClick to Header.js
-*/
-
-function Header({ isDarkMode, onDarkModeClick }) {
-
+const Header = ({ darkMode, onDarkModeClick }) => {
     return (
         <header>
-            <h2>Shopster</h2>
-            <button isDarkMode={ isDarkMode } onClick={ onDarkModeClick }>
-            {!isDarkMode ? "Dark" : "Light"} Mode
-            </button>
-      </header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {darkMode ? "Dark" : "Light"} Mode
+        </button>
+        </header>
     )
 }
 
